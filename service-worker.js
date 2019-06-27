c4c35d797ed8432077883a056640ea7a9253211e
TomislavBatar <wmbl.info@gmail.com>
2019-06-27 14:13:17 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/aura/precache-manifest.77239ee093365905f2dd931d60ed8e98.js"
+  "/aura/precache-manifest.48a56c5413b8d7f95ed83a53fc264183.js"
 );
 
 self.addEventListener('message', (event) => {
