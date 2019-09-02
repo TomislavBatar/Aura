bd18ef230905b2844f16c06d81e4924d9ca64625
TomislavBatar <wmbl.info@gmail.com>
2019-09-02 08:59:57 +0200

    sve kartice podesena rotacija

--- src/sections/sectionsPocetna/Usluge/Usluge.jsx
+++ src/sections/sectionsPocetna/Usluge/Usluge.jsx
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
-import { Row, Col, Container, Image, Card, CardGroup } from 'react-bootstrap';
-import ReactCardFlip from 'react-card-flip';
+import { Row, Col, Container, CardGroup } from 'react-bootstrap';
 import ReactTooltip from 'react-tooltip';
 import GrafikUsluge from './svgIkonice/grafikUsluge';
 
 import './cssUsluge/usluge.css';
 import './cssUsluge/media-query-usluge.css';
 
-// import imgGrafikUsluge from './imgUsluge/GrafikUsluge.svg';
-
-// import imgAutoodgovornost from './imgUsluge/auto.svg';
-import imgKaskoOsiguranje from './imgUsluge/kasko.svg';
-import imgOsiguranjeImovine from './imgUsluge/imovina.svg';
-import imgPutnoZdravstvenoOsiguranje from './imgUsluge/putno.svg';
-import imgOsiguranjeOdNezgode from './imgUsluge/nezgoda.svg';
-import imgOsiguranjekorisnikaKredita from './imgUsluge/kredit.svg';
-
 import KarticaAutoodgovornost from './karticeUsluge/KarticaAutoodgovornost';
 import KarticaKasko from './karticeUsluge/KarticaKasko';
 import KarticaImovina from './karticeUsluge/KarticaImovina';
 import KarticaPutnoOsiguranje from './karticeUsluge/KarticaPutnoOsiguranje';
+import KarticaNezgoda from './karticeUsluge/KarticaNezgoda';
+import KarticaKredit from './karticeUsluge/KarticaKredit';
 export default class Usluge extends Component {
 	constructor() {
 		super();
@@ -47,7 +39,7 @@ export default class Usluge extends Component {
 								<hr className="hr-style" /> <br />
 							</Col>
 							<Col lg={9}>
-								<p data-tip="hello world" className="p-title-usluge">
+								<p className="p-title-usluge">
 									Osiguranje Aura AD Banja Luka je domaće osiguravajuće društvo, registrovano za
 									pružanje usluga neživotnog osiguranja, koje, pored obaveznog osiguranja od
 									autoodgovornosti i kasko osiguranja, pruža i usluge osiguranja imovine, putničkog
@@ -56,12 +48,6 @@ export default class Usluge extends Component {
 								</p>
 							</Col>
 							<Col lg={12} className="radial-gradient">
-								{/* <Image
-									className="grafik-usluge"
-									src={SvgComponent}
-									fluid
-									alt="Aura osiguranje grafik"
-								/> */}
 								<GrafikUsluge />
 							</Col>
 
@@ -74,76 +60,13 @@ export default class Usluge extends Component {
 								<div className="card-grafik-usluge-padding">
 									<CardGroup className="card-group-1">
 										<KarticaAutoodgovornost />
-										{/* <Card className="card-grafik-usluge text-center card-grafik-usluge-bottom-right-border">
-											<Card.Body className="card-body-grafik-usluge">
-												<Image
-													className="grafik-usluge-2"
-													src={imgKaskoOsiguranje}
-													fluid
-													alt="Aura osiguranje grafik"
-												/>
-												<Card.Title className="card-title-grafik-usluge grafik-2-tekst">
-													kasko osiguranje
-												</Card.Title>
-											</Card.Body>
-										</Card> */}
 										<KarticaKasko />
-										{/* <Card className="card-grafik-usluge card-grafik-usluge-top-right-radius card-grafik-usluge-bottom-border text-center">
-											<Card.Body className="card-body-grafik-usluge">
-												<Image
-													className="grafik-usluge-3"
-													src={imgOsiguranjeImovine}
-													fluid
-													alt="Aura osiguranje grafik"
-												/>
-												<Card.Title className="card-title-grafik-usluge grafik-3-tekst">
-													osiguranje imovine
-												</Card.Title>
-											</Card.Body>
-										</Card> */}
 										<KarticaImovina />
 									</CardGroup>
 									<CardGroup className="card-group-1">
-										{/* <Card className="card-grafik-usluge card-grafik-usluge-bottom-left-radius card-grafik-usluge-right-border text-center">
-											<Card.Body className="card-body-grafik-usluge">
-												<Image
-													className="grafik-usluge-4"
-													src={imgPutnoZdravstvenoOsiguranje}
-													fluid
-													alt="Aura osiguranje grafik"
-												/>
-												<Card.Title className="card-title-grafik-usluge grafik-4-tekst">
-													putno zdravstveno osiguranje
-												</Card.Title>
-											</Card.Body>
-										</Card> */}
 										<KarticaPutnoOsiguranje />
-										<Card className="card-grafik-usluge card-grafik-usluge-right-border">
-											<Card.Body className="card-body-grafik-usluge text-center">
-												<Image
-													className="grafik-usluge-5"
-													src={imgOsiguranjeOdNezgode}
-													fluid
-													alt="Aura osiguranje grafik"
-												/>
-												<Card.Title className="card-title-grafik-usluge grafik-5-tekst">
-													osiguranje od nezgoda
-												</Card.Title>
-											</Card.Body>
-										</Card>
-										<Card className="card-grafik-usluge card-grafik-usluge-bottom-right-radius text-center">
-											<Card.Body className="card-body-grafik-usluge">
-												<Image
-													className="grafik-usluge-6"
-													src={imgOsiguranjekorisnikaKredita}
-													fluid
-													alt="Aura osiguranje grafik"
-												/>
-												<Card.Title className="card-title-grafik-usluge grafik-6-tekst">
-													osiguranje korisnika kredita
-												</Card.Title>
-											</Card.Body>
-										</Card>
+										<KarticaNezgoda />
+										<KarticaKredit />
 									</CardGroup>
 								</div>
 							</Col>
---
+++ src/sections/sectionsPocetna/Usluge/karticeUsluge/KarticaKredit.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { Card, Image } from 'react-bootstrap';
+import ReactCardFlip from 'react-card-flip';
+import imgKredit from '../imgUsluge/kredit.svg';
+
+export default class KarticaKredit extends Component {
+	constructor() {
+		super();
+		this.state = {
+			isFlipped: false
+		};
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	handleClick(e) {
+		e.preventDefault();
+		this.setState((prevState) => ({ isFlipped: !prevState.isFlipped }));
+	}
+
+	render() {
+		return (
+			<Card className="card-grafik-usluge-5 card-grafik-usluge-bottom-right-radius text-center">
+				<ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="vertical">
+					<Card
+						data-tip
+						data-for="auto"
+						key="front"
+						className="card-grafik-usluge card-grafik-usluge-bottom-right-radius card-grafik-usluge-right-border text-center no-copy"
+					>
+						<Card.Body onClick={this.handleClick} className="card-body-grafik-usluge">
+							<Image className="grafik-usluge-5" src={imgKredit} fluid alt="Aura osiguranje grafik" />
+
+							<Card.Title className="card-title-grafik-usluge grafik-5-tekst">
+								Osiguranje korisnika kredita
+							</Card.Title>
+						</Card.Body>
+					</Card>
+					<Card
+						onClick={this.handleClick}
+						key="back"
+						className="card-grafik-usluge-back card-grafik-usluge-bottom-right-radius card-grafik-usluge-bottom-right-border text-center no-copy"
+					>
+						<Card.Body className="card-body-grafik-usluge pozicija-tekst-back">
+							<p className="card-grafik-usluge-back-tekst">
+								Osiguranje od auto-odgovornosti je zakonski obavezno osiguranje koje se zaključuje
+								prilikom registracije motornih vozila. Ovim osiguranjem je pokrivena odgovornost
+								vlasnika, odnosno korisnika, kada upotrebom motornog vozila pričini štetu trećim licima.
+							</p>
+							<a className="link-kartica-usluge" href="/aa">
+								Prikaži još detalja...
+							</a>
+						</Card.Body>
+					</Card>
+				</ReactCardFlip>
+			</Card>
+		);
+	}
+}
