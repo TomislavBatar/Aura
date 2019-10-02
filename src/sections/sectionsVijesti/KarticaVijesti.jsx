7c759a541f4f6f6aba799fa0cb288c0f58ff9b44
TomislavBatar <wmbl.info@gmail.com>
2019-10-02 14:56:03 +0200

    šablon kartice

--- src/sections/sectionsVijesti/KarticaVijesti.jsx
+++ src/sections/sectionsVijesti/KarticaVijesti.jsx
@@ -7,7 +7,7 @@ export default class KarticaVijesti extends Component {
 			<React.Fragment>
 				<Card className="kartica-vijest">
 					<Card.Img className="top-radius" variant="top" src={this.props.slika} />
-					<Card.Body>
+					<Card.Body className="sadrzaj-kartica">
 						<Card.Title className="naslov-vijest">{this.props.naslov}</Card.Title>
 						<Card.Text className="tekst-vijest">{this.props.tekst}</Card.Text>
 						<Button className="button-vijest" variant="primary">
--- src/sections/sectionsVijesti/SekcijaVijesti.jsx
+++ src/sections/sectionsVijesti/SekcijaVijesti.jsx
@@ -19,25 +19,51 @@ export default class SekcijaVijesti extends Component {
 							<h1 class="naslov-vijesti">Vijesti</h1>
 						</Col>
 					</Row>
+					{/* row 1 */}
+					<Row className="row-vijesti">
+						<Col lg={4} md={6}>
+							<KarticaVijesti
+								naslov="Obavještenje o radu šalter sala za registraciju motornih vozila"
+								slika="http://auraosiguranje.com/wp-content/uploads/2016/03/aura-osiguranje-capital-640x520.jpg"
+								tekst="Obavještenje o radu šalter sala za registraciju motornih vozila (PDF) Obavještenje o radu šalter sala za registraciju motornih vozila (PDF)"
+							/>
+						</Col>
+						<Col lg={4} md={6}>
+							<KarticaVijesti
+								naslov="Obavještenje o radu šalter sala za registraciju motornih vozila"
+								slika="http://auraosiguranje.com/wp-content/uploads/2016/03/davidvaro-1485510857-parking.jpg"
+								tekst="Obavještenje o radu šalter sala za registraciju motornih vozila (PDF) Obavještenje o radu šalter sala za registraciju motornih vozila (PDF)"
+							/>
+						</Col>
+						<Col lg={4} md={6}>
+							<KarticaVijesti
+								naslov="Obavještenje o radu šalter sala za registraciju motornih vozila"
+								slika="http://auraosiguranje.com/wp-content/uploads/2016/03/baby-caucasian-child-daughter-53571.jpeg"
+								tekst="Obavještenje o radu šalter sala za registraciju motornih vozila (PDF) Obavještenje o radu šalter sala za registraciju motornih vozila (PDF)"
+							/>
+						</Col>
+					</Row>
+
+					{/* row 2 */}
 					<Row>
-						<Col lg={4}>
+						<Col lg={4} md={6}>
 							<KarticaVijesti
 								naslov="Obavještenje o radu šalter sala za registraciju motornih vozila"
-								slika={vijest1}
+								slika="http://auraosiguranje.com/wp-content/uploads/2016/03/singletrip730x411.jpg"
 								tekst="Obavještenje o radu šalter sala za registraciju motornih vozila (PDF) Obavještenje o radu šalter sala za registraciju motornih vozila (PDF)"
 							/>
 						</Col>
-						<Col lg={4}>
+						<Col lg={4} md={6}>
 							<KarticaVijesti
 								naslov="Obavještenje o radu šalter sala za registraciju motornih vozila"
-								slika={vijest1}
+								slika="http://auraosiguranje.com/wp-content/uploads/2016/03/1013-2.jpg"
 								tekst="Obavještenje o radu šalter sala za registraciju motornih vozila (PDF) Obavještenje o radu šalter sala za registraciju motornih vozila (PDF)"
 							/>
 						</Col>
-						<Col lg={4}>
+						<Col lg={4} md={6}>
 							<KarticaVijesti
 								naslov="Obavještenje o radu šalter sala za registraciju motornih vozila"
-								slika={vijest1}
+								slika="http://auraosiguranje.com/wp-content/uploads/2017/08/familytravelingthroughairport.jpg"
 								tekst="Obavještenje o radu šalter sala za registraciju motornih vozila (PDF) Obavještenje o radu šalter sala za registraciju motornih vozila (PDF)"
 							/>
 						</Col>
