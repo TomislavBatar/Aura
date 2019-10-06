dad582d42f6094267d921fecfc91b46dd613d502
TomislavBatar <wmbl.info@gmail.com>
2019-10-06 19:18:34 +0200

    autoodgovornost stranica

--- src/sections/sectionsProizvodi/ProizvodiSablon.jsx
+++ src/sections/sectionsProizvodi/ProizvodiSablon.jsx
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
-
+import { Container, Row, Col, Image } from 'react-bootstrap';
+import './cssProizvodi/proizvodi.css';
+import proizvodislika from '../../components/HomeSlider/imgSlider/bg-home-min.jpg';
 export default class ProizvodiSablon extends Component {
 	render() {
 		return (
-			<div>
-				<div>
-					<p>aaa</p>
+			<div className="sekcija-proizvodi">
+				<div className="bg-proizvodi">
+					<Container>
+						<Row>
+							<Col>
+								<h1 className="naslovi-proizvodi">Autoodgovornost</h1>
+							</Col>
+						</Row>
+					</Container>
+					<Image fluid className="slika-proizvodi" src={proizvodislika} alt="" />
 				</div>
 			</div>
 		);
